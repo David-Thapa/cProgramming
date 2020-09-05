1a676608529108673b36ff8d04487c08826e6412
David Thapa <daybeat06@gmail.com>
2020-09-05 17:09:25 +0545

    Added a c++ file

---
+++ understandingConstructor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class person
+{
+	public: 
+	person()
+	{
+		cout<<"Hey!, Iam David.\n"<<"What's your.\n";
+	}
+};
+int main()
+{
+	person p;
+	char d[50];
+	cin>>d;
+	cout<<"\nHello "<<d;	
+}
